3b01b6c6f15da9b6b0891876cfe1d9b67e85fb8f
VRaj361 <vrajnpatel16@gmail.com>
2022-03-10 16:03:02 +0530

    SignupServlet

--- src/main/java/com/controller/SignupServlet.java
+++ src/main/java/com/controller/SignupServlet.java
@@ -3,12 +3,21 @@ package com.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SignupServlet extends HttpServlet{
+	@Override
+	public void init(ServletConfig con) throws ServletException {
+	String email=con.getInitParameter("email");
+	System.out.println(email);
+	String password=con.getInitParameter("password");
+	System.out.println(password);
+	}
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Run");
@@ -18,6 +27,10 @@ public class SignupServlet extends HttpServlet{
 		System.out.println(firstName+" "+email+" "+password);
 		
 		//produce the content using servlet
+		System.out.println("context value");
+		ServletContext con=request.getServletContext();
+		System.out.println(con.getInitParameter("email"));
+		System.out.println(con.getInitParameter("email"));
 		
 		response.setContentType("text/html");//MIME tag means text and html file
 		PrintWriter out=response.getWriter();
